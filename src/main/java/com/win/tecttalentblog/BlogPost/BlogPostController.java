7281ef65c40ade77ef5d9600a186f517a2978a99
tempesttswinson <tempestt.swinson@gmail.com>
2020-06-23 11:28:03 -0400

    fixed errors and added styling

--- src/main/java/com/win/tecttalentblog/BlogPost/BlogPostController.java
+++ src/main/java/com/win/tecttalentblog/BlogPost/BlogPostController.java
@@ -1,5 +1,8 @@
 package com.win.tecttalentblog.BlogPost;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,17 +15,25 @@ public class BlogPostController {
     @Autowired
     private BlogPostRepository blogPostRepository;
 
-    @GetMapping
-    public String index(BlogPost blogPost) {
+    private static List<BlogPost> posts = new ArrayList<>();
+
+    @GetMapping(value = "/")
+    public String index(BlogPost blogPost, Model model) {
+        model.addAttribute("posts", posts);
         return "blogpost/index";
     }
 
     private BlogPost blogpost;
 
-    @PostMapping
+    @GetMapping(value = "/blogposts/new")
+    public String newBlog(BlogPost blogPost) {
+        return "blogpost/new";
+    }
+
+    @PostMapping(value = "/blogposts")
     public String addNewBlogPost(BlogPost blogPost, Model model) {
         blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
-
+        posts.add(blogPost);
         model.addAttribute("title", blogPost.getTitle());
         model.addAttribute("author", blogPost.getAuthor());
         model.addAttribute("blogEntry", blogPost.getBlogEntry());
